fbd0705d37d58ff0ca397c6a3ab605b2093d552b
pedro <pedro.gregorio96@gmail.com>
2023-01-31 19:03:18 +0000

    login working

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 import GamesTable from "./GameTable";
-import { Route } from 'react-router-dom';
+import Login from "./Login";
+import { useState } from "react";
+import axios from "axios";
 
-function App() {
-  return (<GamesTable />);
-}
+const App: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = async (username: string, password: string) => {
+    try {
+      console.log(username + ", " + password);
+      const response = await axios.post(
+        "http://localhost:8080/login?username=" + username + "&password=" + password,);
+      console.log(response.data);
+      if (response.data == "login") {
+        console.log("sucesso na auth");
+        setIsLoggedIn(true);
+      } else {
+        // Handle failed login
+      }
+    } catch (error) {
+      // Handle network error
+    }
+  };
+
+  return (
+    <div>
+      {isLoggedIn ? (
+        <GamesTable />
+      ) : (
+        <Login onLogin={handleLogin} />
+      )}
+    </div>
+  );
+};
 
 export default App;
---
+++ src/Login.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+interface Props {
+  onLogin: (username: string, password: string) => void;
+  error?: string;
+}
+
+const Login: React.FC<Props> = ({ onLogin, error }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onLogin(username, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <button type="submit">Login</button>
+      </div>
+      {error && <p>{error}</p>}
+    </form>
+  );
+};
+
+export default Login;
